2621500dced0f16e8a122fbead89424dc9550e55
Slals <jonathan@blocs.fr>
2022-07-28 02:20:27 +0200

    Adds recursion

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cmath import exp
 import opcode
 from manim import *
 from enum import Enum
@@ -30,6 +31,7 @@ class OPCODE(Enum):
     OP_15 = 95
     OP_16 = 96
     OP_IF = 99
+    OP_ENDIF = 103
     OP_VERIFY = 105
     OP_DUP = 118
     OP_EQUAL = 135
@@ -42,14 +44,17 @@ class OPCODE(Enum):
     OP_HASH160 = 169
     OP_HASH256 = 170
     OP_CHECKSIG = 172
+    
+stack_bottom_margin = DOWN / 5
 
 class AnimOPCODESeq(Scene):
-    # Must do something recursive
     def construct(self):
         # TODO check if stack valid
-        self.in_stack = [OPCODE.OP_EQUAL, OPCODE.OP_IF, [OPCODE.OP_1, OPCODE.OP_IF, [OPCODE.OP_2]]]
+        self.in_stack = [OPCODE.OP_TRUE, OPCODE.OP_IF, [OPCODE.OP_2], OPCODE.OP_ENDIF, OPCODE.OP_DUP, OPCODE.OP_10, OPCODE.OP_ADD, OPCODE.OP_EQUALVERIFY]
         self.output_stack = []
         self.input_block_level = -1
+        self.current_in_stack_mobj_idx = -1
+        self.tx_invalid = False
         
         self.in_stack_mobj = []
         self.out_stack_mobj = []
@@ -57,89 +62,112 @@ class AnimOPCODESeq(Scene):
         self.explain_mobj = MarkupText(f"Processing the SCRIPT").scale(0.4).to_corner(DOWN)
         self.play(FadeIn(self.explain_mobj))
             
-        self.process_input_block(self.in_stack)
+        self.render_input_stack(self.in_stack)
+        self.render_output_stack(self.in_stack)
         
-        return
+        last_output = self.output_stack[-1]
         
-        last_output = self.output_stack.pop()
-        if last_output == OPCODE.OP_VERIFY:
-            end_text = MarkupText(f'This transaction fails because of <b><span fgcolor="{YELLOW}">OP_VERIFY</span></b>')
-        elif last_output == OPCODE.OP_EQUALVERIFY:
-            value_1, value_2 = self.read_output_stack_params(self.output_stack, 2)
-            end_text = MarkupText(f'The values {value_1} and {value_2} are different. The transaction fails because it does not pass the {OPCODE.OP_VERIFY.name}')
+        if self.tx_invalid:
+            end_text = MarkupText(f'This transaction fails because of <b><span fgcolor="{YELLOW}">{last_output.name}</span></b>')
         else:
             end_text = MarkupText(f'The Result is <b><span fgcolor="{YELLOW}">{last_output}</span></b>')
             
         self.play(Transform(self.explain_mobj, end_text))
         
-    def process_input_block(self, input_block):
+    def render_input_stack(self, input_block):
         self.input_block_level += 1
-        
+
         for idx, in_stack_value in enumerate(input_block):
             if type(in_stack_value) == list:
-                self.process_input_block(in_stack_value)
-                break
+                self.render_input_stack(in_stack_value)
+                continue
             name = f"&lt;{in_stack_value}&gt;"
             if type(in_stack_value) is not str:
                 name = f"{in_stack_value.name}"
             opcode_text = MarkupText(name).scale(0.6).to_corner(UL)
             if len(self.in_stack_mobj) > 0:
-                opcode_text.next_to(self.in_stack_mobj[len(self.in_stack_mobj) - 1], DOWN / 5).align_to(self.in_stack_mobj[idx - 1], LEFT).shift(RIGHT * self.input_block_level / 4)
+                # TODO fixes end of block indent
+                opcode_text.next_to(self.in_stack_mobj[len(self.in_stack_mobj) - 1], stack_bottom_margin).align_to(self.in_stack_mobj[idx - 1], LEFT).shift(RIGHT * self.input_block_level / 4)
             self.play(Write(opcode_text))
             self.in_stack_mobj.append(opcode_text)
             
         self.input_block_level -= 1
-            
-        # TODO recursive output with IF/ELSE blocks
-        return
+    
+    def render_output_stack(self, input_block):
+        for idx, in_stack_value in enumerate(input_block):
+            if self.tx_invalid:
+                break
+
+            # a list a new block, it's been processed by recursion when processing OPCODE
+            # see enter_next_input_block
+            if type(in_stack_value) == list:
+                continue
         
-        for idx, in_stack_value in enumerate(self.in_stack):
-            new_output_stack, explain, tx_invalid = self.process_opcode_output_stack(
+            self.current_in_stack_mobj_idx += 1
+
+            read_output_values, write_output_values, explain, enter_next_input_block = self.process_opcode_output_stack(
                 p_opcode=in_stack_value,
-                p_output_stack=self.output_stack.copy(),
-                current_index=idx
+                input_block=input_block,
+                current_in_idx=idx
             )
             
             new_explain_mobj = MarkupText(explain).scale(0.4).to_corner(DOWN)
             self.play(
-                Indicate(in_stack_mobj[idx]),
-                Transform(explain_mobj, new_explain_mobj)
+                Indicate(self.in_stack_mobj[self.current_in_stack_mobj_idx]),
+                Transform(self.explain_mobj, new_explain_mobj)
             )
             
-            if tx_invalid:
-                self.output_stack = new_output_stack
-                break
+            self.update_output_mobj(read_output_values, write_output_values)
+        
+            if enter_next_input_block:
+                self.render_output_stack(input_block[idx + 1])
+                continue
 
-            output_text = MarkupText(f"{str(new_output_stack[len(new_output_stack) - 1])}").scale(0.6)
+            self.remove(self.explain_mobj)
+            self.explain_mobj = new_explain_mobj
             
-            if idx == 0:
-                output_text.next_to(in_stack_mobj[idx], RIGHT)
+        return read_output_values
+    
+    def update_output_mobj(self, read_values, write_values):            
+        if self.tx_invalid:
+            return
+
+        output_text = None
+        if len(write_values) > 0:
+            output_text = MarkupText(f"{str(write_values[-1])}").scale(0.6)
+        
+        if output_text is not None:
+            if self.current_in_stack_mobj_idx == 0:
+                output_text.next_to(self.in_stack_mobj[0], RIGHT)
             else:
-                output_text.next_to(out_stack_mobj[idx - 1], DOWN).align_to(out_stack_mobj[idx - 1], LEFT)
+                output_text.next_to(self.out_stack_mobj[-1], stack_bottom_margin).align_to(self.out_stack_mobj[-1], LEFT)
 
             self.play(Write(output_text))
-            out_stack_mobj.append(output_text)
+            self.out_stack_mobj.append(output_text)
             
-            if len(new_output_stack) <= len(self.output_stack):
-                nb_unstack = len(self.output_stack) - len(new_output_stack) + 1
-                if nb_unstack == 2:
-                    self.play(
-                        FadeOut(out_stack_mobj[len(out_stack_mobj) - 2], shift=RIGHT),
-                        FadeOut(out_stack_mobj[len(out_stack_mobj) - 3], shift=RIGHT),
-                        out_stack_mobj[len(out_stack_mobj) - 1].animate.move_to(out_stack_mobj[len(out_stack_mobj) - 3]).align_to(out_stack_mobj[len(out_stack_mobj) - 3], LEFT)
-                    )
-            
-            self.output_stack = new_output_stack
+        stack_grp = Group()
+        last_mobj = None
+        for i in range(len(read_values)):
+            last_mobj = self.out_stack_mobj[len(self.out_stack_mobj) - (i + 2)]
+            stack_grp.add(last_mobj)
 
-            self.remove(explain_mobj)
-            explain_mobj = new_explain_mobj
-        
-        self.input_block_level -= 1
+        if len(read_values) > 0:
+            # Update the stack it read values and write some
+            if len(write_values) > 0:
+                self.play(
+                    FadeOut((stack_grp), shift=RIGHT),
+                    self.out_stack_mobj[-1].animate.move_to(last_mobj).align_to(last_mobj, LEFT)
+                )
+            # Only removes the read stack
+            else:
+                self.play(FadeOut((stack_grp), shift=RIGHT))
 
-    def process_opcode_output_stack(self, p_opcode, p_output_stack, current_index):
-        explain = "Nothing happened"
+    def process_opcode_output_stack(self, p_opcode, input_block, current_in_idx):
+        explain = None
         output = None
-        tx_invalid = False
+        enter_next_input_block = False
+        read_output_values = []
+        write_output_values = []
         if p_opcode == OPCODE.OP_0 or p_opcode == OPCODE.OP_1 or p_opcode == OPCODE.OP_2 or p_opcode == OPCODE.OP_3 or p_opcode == OPCODE.OP_4 or p_opcode == OPCODE.OP_5 or p_opcode == OPCODE.OP_5 or p_opcode == OPCODE.OP_6 or p_opcode == OPCODE.OP_7 or p_opcode == OPCODE.OP_8 or p_opcode == OPCODE.OP_9 or p_opcode == OPCODE.OP_10 or p_opcode == OPCODE.OP_11 or p_opcode == OPCODE.OP_12 or p_opcode == OPCODE.OP_13 or p_opcode == OPCODE.OP_14 or p_opcode == OPCODE.OP_15 or p_opcode == OPCODE.OP_16: # TODO all numbers to 16
             if p_opcode is not OPCODE.OP_0:
                 output = p_opcode.value - 80
@@ -147,86 +175,95 @@ class AnimOPCODESeq(Scene):
                 output = 0
             explain = self.get_explain_for_push_value(p_opcode, output)
         elif p_opcode == OPCODE.OP_EQUAL:
-            value_1, value_2 = self.read_output_stack_params(p_output_stack, 2)
-            output = value_1 == value_2
-            explain = f'{p_opcode.name} : Are the values <b><span fgcolor="{YELLOW}">{value_1}</span></b> and <b><span fgcolor="{YELLOW}">{value_2}</span></b> equals?'
+            read_output_values = self.read_output_stack_params(2)
+            output = read_output_values[0] == read_output_values[1]
+            explain = f'{p_opcode.name} : Are the values <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b> and <b><span fgcolor="{YELLOW}">{read_output_values[1]}</span></b> equals?'
         elif p_opcode == OPCODE.OP_ADD:
-            value_1, value_2 = self.read_output_stack_params(p_output_stack, 2)
-            output = value_1 + value_2
-            explain = f'{p_opcode.name} : Adds the values <b><span fgcolor="{YELLOW}">{value_1}</span></b> and <b><span fgcolor="{YELLOW}">{value_2}</span></b>'
+            read_output_values = self.read_output_stack_params(2)
+            output = read_output_values[0] + read_output_values[1]
+            explain = f'{p_opcode.name} : Adds the values <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b> and <b><span fgcolor="{YELLOW}">{read_output_values[1]}</span></b>'
         elif p_opcode == OPCODE.OP_SUB:
-            value_1, value_2 = self.read_output_stack_params(p_output_stack, 2)
-            output = value_1 - value_2
-            explain = f'{p_opcode.name} : Substacts the values <b><span fgcolor="{YELLOW}">{value_1}</span></b> and <b><span fgcolor="{YELLOW}">{value_2}</span></b>'
+            read_output_values = self.read_output_stack_params(2)
+            output = read_output_values[0] - read_output_values[1]
+            explain = f'{p_opcode.name} : Substacts the values <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b> and <b><span fgcolor="{YELLOW}">{read_output_values[1]}</span></b>'
         elif p_opcode == OPCODE.OP_MUL:
-            value_1, value_2 = self.read_output_stack_params(p_output_stack, 2)
-            output = value_1 - value_2
-            explain = f'{p_opcode.name} : Multiplies the values <b><span fgcolor="{YELLOW}">{value_1}</span></b> and <b><span fgcolor="{YELLOW}">{value_2}</span></b>'
+            read_output_values = self.read_output_stack_params(2)
+            output = read_output_values[0] - read_output_values[1]
+            explain = f'{p_opcode.name} : Multiplies the values <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b> and <b><span fgcolor="{YELLOW}">{read_output_values[1]}</span></b>'
         elif p_opcode == OPCODE.OP_DIV:
-            value_1, value_2 = self.read_output_stack_params(p_output_stack, 2)
-            output = value_1 - value_2
-            explain = f'{p_opcode.name} : Divides the values <b><span fgcolor="{YELLOW}">{value_1}</span></b> and <b><span fgcolor="{YELLOW}">{value_2}</span></b>'
+            read_output_values = self.read_output_stack_params(2)
+            output = read_output_values[0] - read_output_values[1]
+            explain = f'{p_opcode.name} : Divides the values <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b> and <b><span fgcolor="{YELLOW}">{read_output_values[1]}</span></b>'
         elif p_opcode == OPCODE.OP_MOD:
-            value_1, value_2 = self.read_output_stack_params(p_output_stack, 2)
-            output = value_1 - value_2
-            explain = f'{p_opcode.name} : Modulos the values <b><span fgcolor="{YELLOW}">{value_1}</span></b> and <b><span fgcolor="{YELLOW}">{value_2}</span></b>'
+            read_output_values = self.read_output_stack_params(2)
+            output = read_output_values[0] - read_output_values[1]
+            explain = f'{p_opcode.name} : Modulos the values <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b> and <b><span fgcolor="{YELLOW}">{read_output_values[1]}</span></b>'
         elif p_opcode == OPCODE.OP_HASH256:
-            value_1, _ = self.read_output_stack_params(p_output_stack, 1)
+            read_output_values = self.read_output_stack_params(1)
             h = hashlib.sha256()
-            h.update(str.encode(value_1))
+            h.update(str.encode(read_output_values[0]))
             output = h.hexdigest()
-            explain = f'{p_opcode.name} : SHA256 Hashes the value <b><span fgcolor="{YELLOW}">{value_1}</span></b>'
+            explain = f'{p_opcode.name} : SHA256 Hashes the value <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b>'
         elif p_opcode == OPCODE.OP_HASH160:
-            value_1, _ = self.read_output_stack_params(p_output_stack, 1)
-            output = hashlib.new("ripemd160", str.encode(value_1)).hexdigest()
-            explain = f'{p_opcode.name} : RIPEMD160 Hashes the value <b><span fgcolor="{YELLOW}">{value_1}</span></b>'
+            read_output_values = self.read_output_stack_params(1)
+            output = hashlib.new("ripemd160", str.encode(read_output_values[0])).hexdigest()
+            explain = f'{p_opcode.name} : RIPEMD160 Hashes the value <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b>'
         elif p_opcode == OPCODE.OP_DUP:
-            output = p_output_stack[len(p_output_stack) - 1]
+            output = self.output_stack[-1]
             explain = f'{p_opcode.name} : Duplicates the value <b><span fgcolor="{YELLOW}">{output}</span></b>'
         elif p_opcode == OPCODE.OP_VERIFY:
-            value_1, _ = self.read_output_stack_params(p_output_stack, 1)
-            if value_1 != True:
-                tx_invalid = True
-                
-            output = OPCODE.OP_VERIFY
+            read_output_values = self.read_output_stack_params(1)
+            if read_output_values[0] != True:
+                self.tx_invalid = True
+                output = OPCODE.OP_VERIFY
                 
             explain = f'{p_opcode.name} : Verifies if the last output value is <b><span fgcolor="{YELLOW}">True</span></b>'
         elif p_opcode == OPCODE.OP_EQUALVERIFY:
-            value_1, value_2 = self.read_output_stack_params(p_output_stack, 2)
-            tx_invalid = value_1 != value_2
-            if tx_invalid:
-                # Puts the values back to the stack
-                # so we can display them in the fail message
-                p_output_stack.append(value_2)
-                p_output_stack.append(value_1)
+            read_output_values = self.read_output_stack_params(2)
+            self.tx_invalid = read_output_values[0] != read_output_values[1]
+            
+            if self.tx_invalid:
                 output = OPCODE.OP_EQUALVERIFY
             else:
-                output = tx_invalid
-            explain = f'{p_opcode.name} : Checks if <b><span fgcolor="{YELLOW}">{value_1}</span></b> and <b><span fgcolor="{YELLOW}">{value_2}</span></b> are equals. The transaction fails if they are not equals'
+                output = True
+        
+            explain = f'{p_opcode.name} : Checks if <b><span fgcolor="{YELLOW}">{read_output_values[0]}</span></b> and <b><span fgcolor="{YELLOW}">{read_output_values[1]}</span></b> are equals. The transaction fails if they are not equals'
         elif p_opcode == OPCODE.OP_CHECKSIG:
-            pubkey, sig = self.read_output_stack_params(p_output_stack, 2)
+            read_output_values = self.read_output_stack_params(2)
             
             # TODO construct a message
             message = bytes.fromhex("304502201fd8abb11443f8b1b9a04e0495e0543d05611473a790c8939f089d073f90509a022100f4677825136605d732e2126d09a2d38c20c75946cd9fc239c0497e84c634e3dd01")
             
             explain = f'{p_opcode.name} : Checks the signature for message <b><span fgcolor="{YELLOW}">{message}</span></b>'
 
-            verif_key = ecdsa.VerifyingKey.from_string(bytes.fromhex(pubkey), curve=ecdsa.SECP256k1)
+            verif_key = ecdsa.VerifyingKey.from_string(bytes.fromhex(read_output_values[0]), curve=ecdsa.SECP256k1)
             try:
-                verif_key.verify(bytes.fromhex(sig), message, hashlib.sha256, sigdecode=sigdecode_der)
+                verif_key.verify(bytes.fromhex(read_output_values[1]), message, hashlib.sha256, sigdecode=sigdecode_der)
             except ecdsa.BadSignatureError:
                 output = False
         elif p_opcode == OPCODE.OP_IF:
-            value = self.read_output_stack_params(p_output_stack, 1)
-            # TODO must enter a new block
+            read_output_values = self.read_output_stack_params(1)
+            if read_output_values[0] != 0:
+                # enters a new block
+                enter_next_input_block = True
+                explain = f'{p_opcode.name} : Enters the IF block'
+            else:
+                explain = f'{p_opcode.name} : Does not enter the IF block'
+        elif p_opcode == OPCODE.OP_ENDIF:
+            # Does nothing, no output
+            explain = f'{p_opcode.name} : The current IF block ended'
         else:
-            output = self.in_stack[current_index]
+            output = input_block[current_in_idx]
             explain = f'Reads the data <b><span fgcolor="{YELLOW}">{output}</span></b> from input stack'
 
         if output is not None:
-            p_output_stack.append(output)
+            self.output_stack.append(output)
+            write_output_values.append(output)
+            
+        if explain is None:
+            explain = f'Not handled OPCODE, got {p_opcode}, and input block {input_block}'
 
-        return p_output_stack, explain, tx_invalid
+        return read_output_values, write_output_values, explain, enter_next_input_block
     
     def get_explain_for_push_value(self, opcode, value):
         if value == 0:
@@ -235,10 +272,9 @@ class AnimOPCODESeq(Scene):
             value = "1 or True"
         return f'{opcode.name} : Pushes the value <b><span fgcolor="{YELLOW}">{value}</span></b>'
     
-    def read_output_stack_params(self, output_stack, nb_params):
-        value_1 = output_stack.pop()
-        value_2 = None
-        if nb_params > 1:
-            value_2 = output_stack.pop()
+    def read_output_stack_params(self, nb_params):
+        values = []
+        for _ in range(nb_params):
+            values.append(self.output_stack.pop())
 
-        return value_1, value_2
+        return values
